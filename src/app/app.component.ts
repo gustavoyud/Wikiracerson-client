a0d2eba5c23ea3c67aaa3bb67297ba5f37aa5d49
gkotecki <gabrielkotecki@gmail.com>
2022-11-01 18:06:48 -0300

    UPDATE: migrate to lazy-loaded standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<router-outlet></router-outlet>`,
+  styles: [],
 })
-export class AppComponent {
-  title = 'little-sena';
-}
+export class AppComponent {}
